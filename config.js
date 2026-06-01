512b5e7893fd386a5e1dfc005527a4f2f5cf1bd8
rjrh-trainning <ruamjairak.trainningcenter@gmail.com>
2026-06-01 09:43:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const GOOGLE_SHEET_API = "https://script.google.com/macros/s/AKfycbw1bS3fjGQngjLdKssc8yWzmbQ1gKOvTvUxraCU2eIHNxjvyyAj3iIqGcey_ETZK3pk/exec";
 const MAIN_REG_API_URL = GOOGLE_SHEET_API; // 🛠️ เสริมตัวแปรนี้เข้ามาเพื่อรองรับโค้ดเก่าใน checkin.html ไม่ให้หลุดโฟลว์
 
-const CERT_STB_API = "https://script.google.com/macros/s/AKfycby0ebBHlmguiJX9RYqX7cBhKV7Cni42Xn6IsJfbjvS2EIZFrBJpu31fI2gQ2Xm5cOEh/exec";
+const CERT_STB_API = "https://script.google.com/macros/s/AKfycbwPVTBu8ykhFvQQ0vCmAamXikaUC-wAA7DHI7pa1k0Y50sGK2BrLNXYLFcA9kUh76Fh/exec";
 const CERT_BLS_API = "https://script.google.com/macros/s/AKfycbxeH8fwOwWeOh83K5zORNVtq5O1ZnNLZDAB79Ic2wyJWZjWRK9frNkD6_dRs_jl6gNVoA/exec";
 
 // ประกาศตัวแปรเซสชันเพื่อให้เรียกใช้ร่วมกันได้ง่าย
